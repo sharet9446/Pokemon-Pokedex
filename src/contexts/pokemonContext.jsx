9b4b3866573e2f34fcfdd96da061146d90b58a56
sharet9446 <spees97@naver.com>
2025-02-09 19:53:49 +0900

    prop-drilling이랑 추가 함수 통일화

--- src/contexts/pokemonContext.jsx
+++ src/contexts/pokemonContext.jsx
@@ -16,38 +16,25 @@ export function PokemonProvider({ children }) {
   const addPokemon = (e, pokemon) => {
     e.stopPropagation();
 
-    const fullPokemonListToast = () => {
+    if (pokemonChoiceList.some((p) => p.id === pokemon.id)) {
+      toast.info(`"${pokemon.korean_name}"은(는) 이미 선택된 포켓몬입니다.`, {
+        position: "top-center",
+        autoClose: 3000,
+      });
+      return;
+    }
+
+    if (pokemonChoiceList.length < maxPokemon) {
+      setPokemonChoiceList([...pokemonChoiceList, pokemon]);
+      toast.success(`"${pokemon.korean_name}"이(가) 추가되었습니다.`, {
+        position: "top-center",
+        autoClose: 3000,
+      });
+    } else {
       toast.error("포켓몬은 최대 6마리까지 선택 가능합니다.", {
         position: "top-center",
         autoClose: 3000,
       });
-    };
-
-    const duplicatePokemonList = () => {
-      toast.info(
-        `"${duplicationPokemon.korean_name}"은(는) 이미 선택된 포켓몬입니다.`,
-        {
-          position: "top-center",
-          autoClose: 3000,
-        }
-      );
-    };
-
-    // 선택된 포켓몬 중복 확인
-    const duplicationPokemon = pokemonChoiceList.find(
-      (pokemonChoice) => pokemonChoice.id === pokemon.id
-    );
-
-    if (duplicationPokemon) {
-      duplicatePokemonList();
-      return;
-    }
-    // 포켓몬 선택 리스트에 포켓몬 추가
-
-    if (pokemonChoiceList.length < maxPokemon) {
-      setPokemonChoiceList([...pokemonChoiceList, pokemon]);
-    } else {
-      fullPokemonListToast();
       return;
     }
   };
