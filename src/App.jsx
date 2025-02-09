dff021d206dccb2a05813cc6aaea701750b9f8c2
sharet9446 <spees97@naver.com>
2025-02-09 19:36:18 +0900

    추가,삭제 함수 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,33 @@ import Dex from "./pages/Dex";
 import { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 
+const maxPokemon = 6;
+
 function App() {
   const [pokemonChoiceList, setPokemonChoiceList] = useState([]);
-
-  const maxPokemon = 6;
 
   const addPokemon = (e, pokemon) => {
     e.stopPropagation();
 
-    const fullPokemonListToast = () => {
+    if (pokemonChoiceList.some((p) => p.id === pokemon.id)) {
+      toast.info(`"${pokemon.korean_name}"은(는) 이미 선택된 포켓몬입니다.`, {
+        position: "top-center",
+        autoClose: 3000,
+      });
+      return;
+    }
+
+    if (pokemonChoiceList.length < maxPokemon) {
+      setPokemonChoiceList([...pokemonChoiceList, pokemon]);
+      toast.success(`"${pokemon.korean_name}"이(가) 추가되었습니다.`, {
+        position: "top-center",
+        autoClose: 3000,
+      });
+    } else {
       toast.error("포켓몬은 최대 6마리까지 선택 가능합니다.", {
         position: "top-center",
         autoClose: 3000,
       });
-    };
-
-    const duplicatePokemonList = () => {
-      toast.info(
-        `"${duplicationPokemon.korean_name}"은(는) 이미 선택된 포켓몬입니다.`,
-        {
-          position: "top-center",
-          autoClose: 3000,
-        }
-      );
-    };
-
-    // 선택된 포켓몬 중복 확인
-    const duplicationPokemon = pokemonChoiceList.find(
-      (pokemonChoice) => pokemonChoice.id === pokemon.id
-    );
-
-    if (duplicationPokemon) {
-      duplicatePokemonList();
-      return;
-    }
-    // 포켓몬 선택 리스트에 포켓몬 추가
-
-    if (pokemonChoiceList.length < maxPokemon) {
-      setPokemonChoiceList([...pokemonChoiceList, pokemon]);
-    } else {
-      fullPokemonListToast();
       return;
     }
   };
